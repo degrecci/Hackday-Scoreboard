016436032c5584e434439ebc8ac72f394d3cca68
degrecci <hdegrecci@gmail.com>
2018-10-17 18:48:35 -0300

    :zap: Adiciona url com query

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Scoreboard extends Component {
+  state = {
+    content: '',
+  }
   componentDidMount = () => {
-    fetch('https://api.github.com/search/commits?q=author:victorperin', {
+    fetch('https://api.github.com/search/issues?q=-label:invalid+created:2018-09-30T10%3A00%3A00%2B00%3A00..2018-11-01T12%3A00%3A00%2B00%3A00+type:pr+is:public+author:degrecci&per_page=300', {
+      method: 'get',
       headers: new Headers({
         'Accept': 'application/vnd.github.cloak-preview'
       })
     })
-      .then(response => console.log(response))
+      .then(response => {
+        response.json().then(data => this.setState({ content: data}));
+      })
   }
     
   render() {
     return (
       <div>
-        Blaaaaa
+        {JSON.stringify(this.state.content)}
       </div>
     )
   }
