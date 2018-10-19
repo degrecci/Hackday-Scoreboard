90c6a25f7a4e5961e2f722bba7087431f72e42f3
degrecci <hdegrecci@gmail.com>
2018-10-18 22:37:29 -0300

    :zap: Melhora datas em search query

--- src/utils/searchQuery.js
+++ src/utils/searchQuery.js
@@ -1,8 +1,13 @@
 import USERS_LIST from '../constants/usersList';
 
 const authors = USERS_LIST.join('+author:');
-const createdDate = '2018-09-30T10%3A00%3A00%2B00%3A00..2018-11-01T12%3A00%3A00%2B00%3A00';
+
+const startDate = '2018-09-30T10:00:00+00:00';
+const endDate = '2018-11-01T12:00:00+00:00';
+const createdDate = `${encodeURIComponent(startDate)}..${encodeURIComponent(endDate)}`;
+
 const quantityPerPage = '300';
+
 const searchQuery = `?q=-label:invalid+created:${createdDate}+type:pr+is:public+author:${authors}&per_page=${quantityPerPage}`;
 
 export default searchQuery;
