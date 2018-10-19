a825c6fc33827132a4c1d5f3ba0e22707e9e2274
degrecci <hdegrecci@gmail.com>
2018-10-18 21:22:26 -0300

    :zap: Utiliza constantes e adiciona resultados

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { GIT_SEARCH_URL } from './constants/gitSearchUrl';
+import SEARCH_QUERY from './constants/searchQuery';
+import USERS_LIST from './constants/usersList';
 
 export default class Scoreboard extends Component {
   state = {
     content: '',
+    isLoading: true,
   }
+
   componentDidMount = () => {
-    fetch('https://api.github.com/search/issues?q=-label:invalid+created:2018-09-30T10%3A00%3A00%2B00%3A00..2018-11-01T12%3A00%3A00%2B00%3A00+type:pr+is:public+author:degrecci&per_page=300', {
+    fetch(`${GIT_SEARCH_URL}${SEARCH_QUERY}`, {
       method: 'get',
       headers: new Headers({
         'Accept': 'application/vnd.github.cloak-preview'
       })
     })
       .then(response => {
-        response.json().then(data => this.setState({ content: data}));
+        response.json()
+          .then(data => {
+            this.setState({
+              content: data,
+              isLoading: false,
+            });
+          });
       })
   }
-    
+
   render() {
+    const { content } = this.state;
+    console.log(USERS_LIST);
+
     return (
       <div>
-        {JSON.stringify(this.state.content)}
+        <h1>TOTAL DE PULL REQUESTS {content.total_count}</h1>
+        <ul>
+        {USERS_LIST.map(user => {
+          <li>{user}</li>
+        })}
+        </ul>
+        {/* {JSON.stringify(this.state.content)} */}
       </div>
     )
   }
