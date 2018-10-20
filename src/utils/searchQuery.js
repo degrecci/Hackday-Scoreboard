842f5d07a614c495bb333e6f2d98dfe31b7c21f7
Henrique Degrecci <henrique.degrecci@upx.com>
2018-10-20 10:18:39 -0300

    Adianta horario de startDate

--- src/utils/searchQuery.js
+++ src/utils/searchQuery.js
@@ -3,7 +3,7 @@ import USERS_LIST from '../constants/usersList';
 
 const authors = USERS_LIST.join('+author:');
 
-const startDate = moment('8:30', 'HH:mm').format();
+const startDate = moment('7:30', 'HH:mm').format();
 const endDate = moment('17:00', 'HH:mm').format();
 
 const createdDate = `${encodeURIComponent(startDate)}..${encodeURIComponent(endDate)}`;
