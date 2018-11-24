6084f9bbf24e0b609834107ab7d050326440ebd9
degrecci <hdegrecci@gmail.com>
2018-11-24 21:10:54 -0200

    :zap: Query retornando de função

--- src/utils/searchQuery.js
+++ src/utils/searchQuery.js
@@ -1,15 +1,16 @@
 import moment from 'moment';
-import USERS_LIST from '../constants/usersList';
 
-const authors = USERS_LIST.join('+author:');
+const searchQuery = (usersList) => {
+  const authors = usersList.join('+author:');
 
-const startDate = moment('7:30', 'HH:mm').format();
-const endDate = moment('17:00', 'HH:mm').format();
+  const startDate = moment('7:30', 'HH:mm').format();
+  const endDate = moment('17:00', 'HH:mm').format();
 
-const createdDate = `${encodeURIComponent(startDate)}..${encodeURIComponent(endDate)}`;
+  const createdDate = `${encodeURIComponent(startDate)}..${encodeURIComponent(endDate)}`;
 
-const quantityPerPage = '300';
+  const quantityPerPage = '300';
 
-const searchQuery = `?q=-label:invalid+created:${createdDate}+type:pr+is:public+author:${authors}&per_page=${quantityPerPage}`;
+  return `?q=-label:invalid+created:${createdDate}+type:pr+is:public+author:${authors}&per_page=${quantityPerPage}`;
+}
 
 export default searchQuery;
