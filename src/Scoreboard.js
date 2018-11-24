fae82fd35b245d265a521ae430bb95aac9dfb2db
degrecci <hdegrecci@gmail.com>
2018-11-24 21:09:37 -0200

    :wrench: Ajusta scoreboard

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -2,32 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 import { GIT_SEARCH_URL } from './constants/gitSearchUrl';
 import searchQuery from './utils/searchQuery';
-import USERS_LIST from './constants/usersList';
 
 import './Scoreboard.css';
 
-export default function Scoreboard() {
+export default function Scoreboard(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [content, setContent] = useState({});
 
+  const fetchPullRequests = () => fetch(`${GIT_SEARCH_URL}${searchQuery(props.usersList)}`, {
+    method: 'get',
+    headers: new Headers({
+      Accept: 'application/vnd.github.cloak-preview',
+    }),
+  })
+    .then(response => response.json())
+    .then((data) => {
+      setContent(data);
+      setIsLoading(false);
+    });
+
   useEffect(() => {
-    setInterval(
-      () => fetch(`${GIT_SEARCH_URL}${searchQuery}`, {
-        method: 'get',
-        headers: new Headers({
-          Accept: 'application/vnd.github.cloak-preview',
-        }),
-      })
-        .then(response => response.json())
-        .then((data) => {
-          setContent(data);
-          setIsLoading(false);
-        }),
-      120000,
-    );
+    fetchPullRequests();
+    setInterval(fetchPullRequests, 120000);
   }, []);
 
-  function countPRsByUsers(items, user) {
+  const countPRsByUsers = (items, user) => {
     let count = 0;
 
     items.map((item) => {
@@ -38,16 +37,14 @@ export default function Scoreboard() {
     });
 
     return count;
-  }
+  };
 
-  function insertUsersPullRequests(contentToRank) {
-    return USERS_LIST.map(user => ({
-      username: user,
-      pullRequests: countPRsByUsers(contentToRank.items, user),
-    }));
-  }
+  const insertUsersPullRequests = contentToRank => props.usersList.map(user => ({
+    username: user,
+    pullRequests: countPRsByUsers(contentToRank.items, user),
+  }));
 
-  function rankedUsersByPullRequests(contentToRank) {
+  const rankedUsersByPullRequests = (contentToRank) => {
     const userData = insertUsersPullRequests(contentToRank);
 
     const rankedUsers = userData.sort((a, b) => b.pullRequests - a.pullRequests);
@@ -61,30 +58,24 @@ export default function Scoreboard() {
         {rankedUser.pullRequests}
       </li>
     ));
-  }
-
-  const isUsersListEmpty = USERS_LIST.length === 0;
+  };
 
   return (
     <div className="scoreboard">
-      {(!isUsersListEmpty && !isLoading)
-          && (
-          <div>
-            <h1 className="scoreboard__title">PULL REQUEST SCOREBOARD</h1>
-            <ul className="scoreboard__users-list">
-              {rankedUsersByPullRequests(content)}
-            </ul>
-            <h2 className="scoreboard__total-score">
-TOTAL
-              {' '}
-              {content.total_count}
-            </h2>
-          </div>
-          )
-        }
-      {isUsersListEmpty
-          && <h1>ADICIONE USUÁRIOS GITHUB NO ARQUIVO usersList.js</h1>
-        }
+      {!isLoading
+        && (
+        <div>
+          <ul className="scoreboard__users-list">
+            {rankedUsersByPullRequests(content)}
+          </ul>
+          <h2 className="scoreboard__total-score">
+            TOTAL
+            {' '}
+            {content.total_count}
+          </h2>
+        </div>
+        )
+      }
     </div>
   );
 }
