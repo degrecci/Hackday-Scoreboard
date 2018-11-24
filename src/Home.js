8c034bd6c8f5732a6a48f8dd066ede3dc906a0b0
degrecci <hdegrecci@gmail.com>
2018-11-24 21:09:56 -0200

    :zap: Adiciona form com textfield para users

---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Scoreboard from './Scoreboard';
+import './Home.css';
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+  const [usersList, setUsersList] = useState([]);
+
+  const submitUsersList = (e) => {
+    e.preventDefault();
+    setUsersList(users.split(','));
+  };
+
+  const handleUsersChange = (e) => {
+    setUsers(e.target.value);
+  };
+
+  const isUsersListEmpty = usersList.length === 0;
+
+  return (
+    <div>
+      <h1 className="home__title">HACKDAY SCOREBOARD</h1>
+      {isUsersListEmpty
+        && (
+        <form onSubmit={submitUsersList}>
+          <textarea name="users" onChange={handleUsersChange} />
+          <button type="submit">Salvar</button>
+        </form>
+        )
+      }
+      {!isUsersListEmpty
+        && <Scoreboard usersList={usersList} />
+      }
+    </div>
+  );
+}
